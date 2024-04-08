16ca8aa9074eebb564c28a4dbdb02e64177ac720
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-04-09 00:15:53 +0300

    template is now complete
    - Tested compatibility for phones, tablets, desktops and laptops
    - Main content is mostly empty and should be modified to suit specific needs
    - This commit deprecates the auth page template

--- template/avg_page_template/toggleable/show-nav.js
+++ template/avg_page_template/toggleable/show-nav.js
@@ -1,11 +1,17 @@
-isNavShown = true;
+let isNavShown = false;
 
 function toggleNav() {
     if (isNavShown == true) {
         document.getElementById("page-nav").setAttribute("class", "hidden-nav");
+        if (window.innerHeight > window.innerWidth) {
+            document.getElementById("page-main").setAttribute("class", "shown-main");   
+        }
         isNavShown = false;
     } else {
         document.getElementById("page-nav").setAttribute("class", "shown-nav");
+        if (window.innerHeight > window.innerWidth) {
+            document.getElementById("page-main").setAttribute("class", "hidden-main");   
+        }
         isNavShown = true;
     }
 }
