4326ffd53ac6fa98576b619a00a1076ca862bb4d
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-06-19 19:18:48 +0300

    add not found check

--- backend/prisoner_management/service.py
+++ backend/prisoner_management/service.py
@@ -1,4 +1,5 @@
 import constants
+import exceptions
 import utils
 from repository import PrisonerRepository
 
@@ -7,8 +8,9 @@ prisoner_repository = PrisonerRepository(f"dbname={constants.DB_NAME}")
 
 def get_prisoner(prisoner_id: str):
     prisoner = prisoner_repository.find_by_id(condition_value=prisoner_id)
+    if not prisoner:
+        raise exceptions.ServiceException(404, 'Service error: No results found!')
+
     utils.normalise_row(prisoner)
 
     return prisoner
-
-
