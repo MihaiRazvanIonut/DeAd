cf741f06cf1c1427de20054301a7f98c429b14e6
Costin Cazacu <costincazacu2015@gmail.com>
2024-06-21 13:47:40 +0300

    added load photo feature on prisoner page

--- frontend/prisoner_view_template/edit_prisoner.js
+++ frontend/prisoner_view_template/edit_prisoner.js
@@ -1,16 +1,31 @@
 let form = document.getElementById("prisoners_form");
+
 function readOnlyForm() {
     let inputs = form.getElementsByClassName("default-form__input");
+    let fileInputs = form.querySelectorAll("input[type='file']");
+
     [].forEach.call(inputs, function (input) {
         console.log(input);
         input.readOnly = true;
     });
-} 
+
+    [].forEach.call(fileInputs, function (fileInput) {
+        console.log(fileInput);
+        fileInput.disabled = true;
+    });
+}
 
 function editForm() {
     let inputs = form.getElementsByClassName("default-form__input");
+    let fileInputs = form.querySelectorAll("input[type='file']");
+
     [].forEach.call(inputs, function (input) {
         console.log(input);
         input.readOnly = false;
+    });
+
+    [].forEach.call(fileInputs, function (fileInput) {
+        console.log(fileInput);
+        fileInput.disabled = false;
     });
 }
