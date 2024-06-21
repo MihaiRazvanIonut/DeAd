c66e17254a7ee20dfb0384c1480d5efc95e94f77
Costin Cazacu <costincazacu2015@gmail.com>
2024-06-21 13:14:47 +0300

    added load photo to when the form can be edited

--- frontend/visit_view_template/edit_visit.js
+++ frontend/visit_view_template/edit_visit.js
@@ -1,26 +1,43 @@
 let form = document.getElementById("visits_form");
+
 function readOnlyForm() {
     let inputs = form.getElementsByClassName("default-form__input");
     let selects = form.getElementsByTagName("select");
+    let fileInputs = form.querySelectorAll("input[type='file']");
+    
     [].forEach.call(inputs, function (input) {
         console.log(input);
         input.readOnly = true;
     });
+    
     [].forEach.call(selects, function (select) {
         console.log(select);
         select.disabled = true;
+    });
+    
+    [].forEach.call(fileInputs, function (fileInput) {
+        console.log(fileInput);
+        fileInput.disabled = true;
     });
 }
 
 function editForm() {
     let inputs = form.getElementsByClassName("default-form__input");
     let selects = form.getElementsByTagName("select");
+    let fileInputs = form.querySelectorAll("input[type='file']");
+    
     [].forEach.call(inputs, function (input) {
         console.log(input);
         input.readOnly = false;
     });
+    
     [].forEach.call(selects, function (select) {
         console.log(select);
         select.disabled = false;
+    });
+    
+    [].forEach.call(fileInputs, function (fileInput) {
+        console.log(fileInput);
+        fileInput.disabled = false;
     });
 }
