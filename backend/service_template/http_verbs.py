16d9437e2199452ee7603e641b076c18a21b7456
MihaiRazvanIonut <razvan.mihai3781@gmail.com>
2024-06-21 09:32:09 +0300

    delete service template

--- backend/service_template/http_verbs.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class HTTPVerbs(Enum):
-    POST = 'post'
-    GET = 'get'
-    PUT = 'put'
-    DELETE = 'delete'
--- backend/service_template/main.py
+++
@@ -1,6 +0,0 @@
-import constants
-import server
-
-if __name__ == "__main__":
-    server = server.ServerFacade(constants.HOST, constants.PORT)
-    server.run()
